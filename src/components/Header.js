0cae0b44af64f27d99a593fa3bc94460afa7ba68
John Lundy <79663204+johnlundy94@users.noreply.github.com>
2023-06-06 08:54:07 -0600

    Completed nav implementation

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,31 +4,21 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import {
   faGithub,
   faLinkedin,
-  faMedium,
-  faStackOverflow,
 } from "@fortawesome/free-brands-svg-icons";
 import { Box, HStack } from "@chakra-ui/react";
 
 const socials = [
   {
     icon: faEnvelope,
-    url: "mailto: hello@example.com",
+    url: "mailto: johnclundyy@gmail.com",
   },
   {
     icon: faGithub,
-    url: "https://github.com",
+    url: "https://github.com/johnlundy94",
   },
   {
     icon: faLinkedin,
-    url: "https://www.linkedin.com",
-  },
-  {
-    icon: faMedium,
-    url: "https://medium.com",
-  },
-  {
-    icon: faStackOverflow,
-    url: "https://stackoverflow.com",
+    url: "https://www.linkedin.com/in/john-c-lundy",
   },
 ];
 
